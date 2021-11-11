30f69ce5833b05cfa61f6838ca0f2e8b73e3379d
Basile <interbeing34@gmail.com>
2021-11-11 22:32:46 +0300

    Добавил 24 исправленных теста с помощью DataProvider

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,77 +1,123 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
 
 
 public class CalculatorTest {
-    @Test
-    @DisplayName("Позитивный тест. Сложение")
-    void calculatorTestWhenAddictionPositive() {
-        Assertions.assertEquals(100, Calculator.calc(50.0, 50.0, '+'));
+
+    @DataProvider
+    public static Object[][] testingSumDataPositive() {
+        return new Object[][]{
+                {4, 2, 2},
+                {100, 50, 50},
+                {100, 99, 1}
+        };
     }
 
-    @Test
-    @DisplayName("Негативный тест . Сложение")
-    void calculatorTestWhenAddictionNegative() {
-        Assertions.assertNotEquals(101, Calculator.calc(50.0, 50.0, '+'));
-    }
-
-    @Test
-    @DisplayName("Позитивный тест. Вычитание")
-    void calculatorTestWhenSubtractionPositive() {
-        Assertions.assertEquals(0, Calculator.calc(50.0, 50.0, '-'));
-    }
-
-    @Test
-    @DisplayName("Негативный тест. Вычитание")
-    void calculatorTestWhenSubtractionNegative() {
-        Assertions.assertNotEquals(101, Calculator.calc(50.0, 50.0, '-'));
-    }
-
-    @Test
-    @DisplayName("Позитивный тест. Умножение")
-    void calculatorTestWhenMultiplicationPositive() {
-        Assertions.assertEquals(2500, Calculator.calc(50.0, 50.0, '*'));
-    }
-
-    @Test
-    @DisplayName("Негативный тест. Умножение")
-    void calculatorTestWhenMultiplicationNegative() {
-        Assertions.assertNotEquals(101, Calculator.calc(50.0, 50.0, '*'));
-
-    }
-    @Test
-    @DisplayName("Позитивный тест. Деление")
-    void calculatorTestWhenDivizionPositive() {
-        Assertions.assertEquals(1, Calculator.calc(50.0, 50.0, '/'));
-    }
-
-    @Test
-    @DisplayName("Негативный тест. Деление на ноль")
-    void divisionByZero() {
-        double inf = Double.POSITIVE_INFINITY;
-        Assertions.assertEquals(Double.POSITIVE_INFINITY, Calculator.calc(3, 0, '/'));
+    @Test(dataProvider = "testingSumDataPositive")
+    public void testingSumDataPositive(int expectedValue, int one, int two) {
+        Assert.assertEquals(expectedValue, Calculator.calc(one, two, '+'));
     }
 
     @DataProvider
-    public Object [][] testingData (){
-      return new Object[][]{
-              {5, Calculator.calc(2.0,3.0,'+')},
-              {5, Calculator.calc(4.0,1.0,'+')},
-              {5, Calculator.calc(0.0,5.0,'+')}
-      }  ;
+    public static Object[][] testingSumDataNegative() {
+        return new Object[][]{
+                {5, 2, 2},
+                {101, 50, 50},
+                {99, 99, 0}
+        };
     }
 
-    @Test (dataProvider = "testingData")
-    public void testDataProviderAddiction (double result, double num){
-        Assert.assertEquals(5.0,5.0, "всё верно");
-
+    @Test(dataProvider = "testingSumDataNegative")
+    public void testingSumDataNegative(int expectedValue, int one, int two) {
+        Assert.assertNotEquals(expectedValue, Calculator.calc(one, two, '+'));
     }
 
+    @DataProvider
+    public static Object[][] testingSubNegative() {
+        return new Object[][]{
+                {5, 10, 5},
+                {101, 202, 101},
+                {99, 100, 2}
+        };
+    }
+
+    @Test(dataProvider = "testingSubNegative")
+    public void SubNegative(int expectedValue, int one, int two) {
+        Assert.assertNotEquals(expectedValue, Calculator.calc(one, two, '-'));
+    }
+
+    @DataProvider
+    public static Object[][] testingSubPositive() {
+        return new Object[][]{
+                {5, 15, 10},
+                {6, 16, 10},
+                {99, 100, 1}
+        };
+    }
+
+    @Test(dataProvider = "testingSubPositive")
+    public void SubPositive(int expectedValue, int one, int two) {
+        Assert.assertEquals(expectedValue, Calculator.calc(one, two, '-'));
+    }
+
+    @DataProvider
+    public static Object[][] testingMultPositive() {
+        return new Object[][]{
+                {10, 2, 5},
+                {6, 2, 3},
+                {99, 3, 33}
+        };
+    }
+
+    @Test(dataProvider = "testingMultPositive")
+    public void MultPositive(int expectedValue, int one, int two) {
+        Assert.assertEquals(expectedValue, Calculator.calc(one, two, '*'));
+    }
+
+    @DataProvider
+    public static Object[][] testingMultNegative() {
+        return new Object[][]{
+                {11, 2, 5},
+                {61, 2, 3},
+                {919, 3, 33}
+        };
+    }
+
+    @Test(dataProvider = "testingMultNegative")
+    public void MultNegative(int expectedValue, int one, int two) {
+        Assert.assertNotEquals(expectedValue, Calculator.calc(one, two, '/'));
+    }
+
+    @DataProvider
+    public static Object[][] testingDivNegative() {
+        return new Object[][]{
+                {11, 2, 5},
+                {61, 2, 3},
+                {919, 3, 33}
+        };
+    }
+
+    @Test(dataProvider = "testingDivNegative")
+    public void DivNegative(int expectedValue, int one, int two) {
+        Assert.assertNotEquals(expectedValue, Calculator.calc(one, two, '/'));
+    }
+
+    @DataProvider
+    public static Object[][] testingDivPositive() {
+        return new Object[][]{
+                {11, 2, 5},
+                {61, 2, 3},
+                {0, 678, 0}
+        };
+    }
+
+    @Test(dataProvider = "testingDivPositive")
+    public void DivPositive(int expectedValue, int one, int two) {
+        Assert.assertNotEquals(expectedValue, Calculator.calc(one, two, '/'));
+    }
 }
 
 
